2d9c6a4ebb1fac378e6d8988cf4eff5e913e1fea
Francisco <franciscobarros754@gmail.com>
2020-09-10 13:59:11 -0300

    Adicionado dependencia que limpa a pasta public antes de realizar um novo processo

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let config = {
@@ -8,6 +9,7 @@ let config = {
     path: path.resolve(__dirname, 'public'),
   },
   plugins: [
+    new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: ['public'] }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
       minify: false
